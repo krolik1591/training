ec7ae9f544829df5785ff8b7a8b2f750cb444ec1
krolik1591 <100967267+krolik1591@users.noreply.github.com>
2022-03-21 16:58:00 +0200

    1

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function Calculator(){
-    x1;
-    x2;
+    this.x1 = x1;
+    this.x2 = x2;
     
-    read(){
+    this.read = function(){
         this.x1 = prompt('Введите число', '')
         this.x2 = prompt('Ещё разок', '')
     }
 
-    sum(){
+    this.sum = function(){
         return(this.x1 + this.x2)
     }
 
-    mul(){
+    this.mul = function(){
         return(thix.x1 * thix.x2)
     }
 
 }
 
-let calculator() = new Calculator()
+let calculator = new Calculator()
 
 alert( 'Sum=' + calculator.sum() )
 alert( 'Mul=' + calculator.mul() )
