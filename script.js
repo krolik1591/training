5ecb204884206cf6aa7e8b26bddd8403bd39f904
krolik1591 <100967267+krolik1591@users.noreply.github.com>
2022-03-24 22:11:11 +0200

    sudoku

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
-function Accumulator(startingValue){
+function generateNum(x, y){
+    Math.floor(y/3) * x + 1
+    let a = x + y * 3 + Math.floor(y/3)
+    return a%9 + 1
+}
 
-    this.value = +startingValue
+function generateSetka(){
+    let setka = []
 
-    this.num = 0
-    
-    this.read = function(){
-        this.num += +prompt('Увеличить на', '')
-        this.value += this.num
+    for(let y = 0; y < 9; y++){
+        let str = []
+        for(let x = 0; x < 9; x++){
+            str.push(generateNum(x, y))
+        }
+        setka.push(str)
+    }
+    return setka
+}
+
+function printSetka(setka){
+    for(let item of setka){
+        console.log(item)
     }
 }
 
-let accumulator = new Accumulator(1)
+function random(){
+    return Math.floor(Math.random() * 3)
+}
 
-accumulator.read()
-accumulator.read()
+function swapRollSmall(setka){
+    let kvadrat = random()
+    let ryadok = random() + kvadrat * 3
+    let ryadok1 = (ryadok + 1)%3 + kvadrat * 3
+    
 
-alert(accumulator.value)
+    menyauMestami(setka, ryadok, ryadok1)
+
+}
+
+function swapKvadratov(setka){
+    let kvadrat = random()
+    let kvadrat1 = (kvadrat + 1)%3
+    
+    menyauMestami(setka, kvadrat * 3, kvadrat1 * 3)
+    menyauMestami(setka, kvadrat * 3 + 1, kvadrat1 * 3 + 1)
+    menyauMestami(setka, kvadrat * 3 + 2, kvadrat1 * 3 + 2)
+}
+
+function menyauMestami(arr, mesto1, mesto2){
+    let x = arr[mesto1]
+    arr[mesto1] = arr[mesto2]
+    arr[mesto2] = x
+}
+
+setka1 = generateSetka()
+swapRollSmall(setka1)
+printSetka(setka1)
